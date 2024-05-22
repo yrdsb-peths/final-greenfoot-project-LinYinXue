ca18f2a930cce3303fe622bf926af0a9b85d2b77
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-05-22 16:05:11 -0400

    add hero class

---
+++ Hero.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The main character, hero.
+ * 
+ * @author (Lin Yin) 
+ * @version (2024 May)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
