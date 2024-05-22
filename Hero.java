43836be71f069df75cf1f6f9e4c304be2590fdc2
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-05-22 18:32:48 -0400

    edit hero

--- Hero.java
+++ Hero.java
@@ -12,8 +12,26 @@ public class Hero extends Actor
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int jumpHeight = 20;
     public void act()
     {
         // Add your action code here.
+        moveHero();
+    }
+    
+    private void moveHero()
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-3);
+        }
+        if(Greenfoot.isKeyDown("right"))
+        {
+            move(3);
+        }
+        if(Greenfoot.isKeyDown("up"))
+        {
+            setLocation(getX(), getY()-3);
+        }
     }
 }
